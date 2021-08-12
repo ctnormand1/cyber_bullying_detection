8bda8e8b961443749471db825551bf7bf34b9907
Nati Marcus <nati.marcus24@gmail.com>
2021-08-12 16:04:02 -0400

    small tweaks to file organization and streamlit updates

--- project_5_st_app.py
+++ project_5_st_app.py
@@ -10,7 +10,7 @@ page = st.sidebar.selectbox(
 if page == 'Home':
     st.title('Detecting Cyberbullying')
 
-    image = Image.open('Cyberbullying_02.png')
+    image = Image.open('st-images/Cyberbullying_02.png')
     #source: https://www.avast.com/c-cyberbullying
     st.image(image, caption='source: https://www.avast.com/c-cyberbullying')
 
@@ -42,21 +42,21 @@ if page == 'Detection App':
         options = ['an attack','not attack']
         st.write(f'This comment is {sample_output(text,options)}')
         if sample_output(text,options) == "an attack":
-            image = Image.open('attack.png')
+            image = Image.open('st-images/attack.png')
             st.image(image, caption='source: https://emoji.gg/emoji/PikaAttack')
     elif selectbox == "Toxicity Model":
         text = st.text_input("Is this comment toxic?")
         options = ['toxic','not toxic']
         st.write(f'This comment is {sample_output(text,options)}')
         if sample_output(text,options) == "toxic":
-            image = Image.open('toxicity.png')
+            image = Image.open('st-images/toxicity.png')
             st.image(image, caption='source: https://www.emojipng.com/preview/990569')
     elif selectbox == "Aggression Model":
         text = st.text_input("Is this comment aggressive?")
         options = ['aggressive','not aggressive']
         st.write(f'This comment is {sample_output(text,options)}')
         if sample_output(text,options) == "aggressive":
-            image = Image.open('aggression.jpeg')
+            image = Image.open('st-images/aggression.jpeg')
             st.image(image, caption='source: https://www.seekpng.com/ipng/u2q8q8e6o0w7u2e6_aggression-clip-art-angry-and-bite-the-smiley/')
     # with open('models/author_pipe.pkl', mode='rb') as pickle_in:
     #     pipe = pickle.load(pickle_in)
